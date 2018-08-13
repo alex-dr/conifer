adc0a9ba3532ff06b5f79a65d80cd7d14d514b96
alex-dr <alex@datarobot.com>
2018-08-13 00:41:19 -0400

    Add license and make setup.py better

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(name='conifer',
       version='0.0.1.dev0',
       description='Library for loading configuration values from multiple sources',
       author='alex-dr',
+      author_email='alexconway12@gmail.com',
+      url='https://github.com/alex-dr/conifer',
       install_requires=[
           'jsonschema'
       ],
@@ -14,4 +16,17 @@ setup(name='conifer',
           ],
       },
       packages=find_packages(),
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries',
+      ],
+      keywords=[
+          'config', 'configuration'
+      ],
       )
