15350c59105709089f673f0e4f8b48d7fbc0962e
Alex Conway <alex@datarobot.com>
2020-03-05 11:55:30 -0500

    Fix null case

--- conifer/sources/json_file.py
+++ conifer/sources/json_file.py
@@ -39,7 +39,8 @@ class JSONFileLoader(object):
     def _load_data(self):
         if self._path is not None:
             if not os.path.exists(self._path):
-                return {}
+                self._data = {}
+                return
             with open(self._path, "rb") as _fp:
                 self._data = json.load(_fp)
         else:
